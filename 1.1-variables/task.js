701aa427a7d14b98343d3e46f8592d9bb878d1ac
Шабанов Касум <shabanov.kasum@list.ru>
2021-02-20 19:28:48 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -8,20 +8,29 @@
 function averageMark(a, g, p) {
 	
     // реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
     
-	return // ваш ответ;
+	return((a + g + p) / 3);// ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+  let myName = userName;
+	let message = "Привет, мир! Меня зовут";
+
+	return(message + " " + myName);// ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+  let x = 2;
+	let y = 22;
+	let z = 0;  
+	let result = (x * y) + (5 * z) + x - 1;
+
+  return(result) // ваш ответ;
 }
