aec51010ae448d290cde4ed138c6215d9f32c607
Шабанов Касум <shabanov.kasum@list.ru>
2021-02-28 17:49:32 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,39 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let D = b**2 -4*a*c; // формула дискримента
+
+
+    if (D < 0) {
+    } else if (D == 0) {
+        x.push((-b + Math.sqrt(D)) / (2 *a));
+    } else (D > 0) {
+        x.push((-b + Math.sqrt(D)) / (2 *a));
+        x.push((-b - Math.sqrt(D)) / (2 *a));
+    };
+     return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    // return averageMark;
+  let ratings = [];
+
+  if (ratings == 0) {
+      return 0
+  } else if (ratings > 5) {
+    ratings.slice(0,4);
+    return ("Вы хотите вывести больше оценок")
+  };
+  
+  let amount = 0;
+  for(let i =0; i = marks.lengts; i++){
+      amount += marks[i];
+  }
+
+  
+
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
