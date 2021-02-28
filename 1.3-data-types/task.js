4edf3a0e5af56d086b2826cf8ec38f14ed65023a
Шабанов Касум <shabanov.kasum@list.ru>
2021-02-28 17:08:46 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,23 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
+    let s = amount - contirbution;
+    let p = (percent/100)/12;
+
+    let formAmount = s * (percent + percent / (Math.pow(1 + p, date)) -1);
+
+    let = totalAmount = formAmount/100;
+    return +totalAmount.toFixed(2)
     // return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
+//let a = "Аноним";
+
+    if (name === "") {
+        return (`Привет, мир! Меня зовут Аноним.`)
+    } else  {
+        return (`Привет, мир! Меня зовут ${name}.`)
+    };
     // return greeting;
 }
